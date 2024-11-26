6f773bf4a92b3a8c3843c0e053c951b929725d42
Nick Doucette <nickdoucette24@gmail.com>
2024-11-26 01:51:08 -0500

    Hero Section started, rotating cube pretty much done, initial flexbox built but tons of work to go.

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,7 +1,36 @@
 import "./LandingPage.scss";
+// import headshot from "../../assets/images/headshot.svg";
 
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <section className="landing-page">
+      <div className="headshot-wrapper">
+        <div className="headshot-wrapper__container">
+          <img
+            className="headshot-wrapper__image"
+            // src={headshot}
+            alt="headshot photo of Nick, the one who made this website"
+          />
+        </div>
+        <div className="landing-page__text-container">
+          <h1 className="landing-page__text-container--h1">
+            Hi, my name is <span className="">Nick</span>
+          </h1>
+          <div className="landing-page__cube-container">
+            <h2 className="landing-page__text-container--h2">I am a</h2>
+            <div className="landing-page__rotating-cube">
+              <div className="cube">
+                <div className="face front">software engineer</div>
+                <div className="face top">web developer</div>
+                <div className="face back">UX designer</div>
+                <div className="face bottom">creative developer</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default LandingPage;
