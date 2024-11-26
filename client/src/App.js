dacfc6d38694ece493dfdba5c952b6007d303358
Nick Doucette <nickdoucette24@gmail.com>
2024-11-25 22:52:50 -0500

    Header built for mobile and laptop breakpoint. Still need to do desktop breakpoint, more touches, and more functionality. Links and useNavigate working, button and link animations done, and still need to do onScroll background colour change

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./sass/App.scss";
+import Header from "./components/Header/Header";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import DoggoPage from "./pages/DoggoPage/DoggoPage";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/forthedogs" element={<DoggoPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
