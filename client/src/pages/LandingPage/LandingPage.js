730b7c8d36df328d7384c247d1af8cc76c48cdc0
Nick Doucette <nickdoucette24@gmail.com>
2024-11-26 17:15:10 -0500

    Landing page responsive up to laptop for the most part. Still lots to do, just saving here.

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,29 +1,87 @@
+import { useState, useEffect } from "react";
 import "./LandingPage.scss";
-// import headshot from "../../assets/images/headshot.svg";
+import headshot from "../../assets/images/headshot.JPG";
 
 const LandingPage = () => {
+  const [borderPosition, setBorderPosition] = useState({ x: 0, y: 0 });
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleMouseMove = (event) => {
+    const img = event.target.getBoundingClientRect();
+    const x = (event.clientX - img.left - img.width / 2) / 20;
+    const y = (event.clientY - img.top - img.height / 2) / 20;
+    setBorderPosition({ x, y });
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsVisible(true);
+          }
+        });
+      },
+      { threshold: 0.25 }
+    );
+
+    const target = document.querySelector(".hero-wrapper__headshot");
+    if (target) {
+      observer.observe(target);
+    }
+    return () => {
+      if (target) {
+        observer.unobserve(target);
+      }
+    };
+  }, []);
+
   return (
     <section className="landing-page">
-      <div className="headshot-wrapper">
-        <div className="headshot-wrapper__container">
+      <div className="hero-wrapper">
+        <div
+          className={`hero-wrapper__headshot ${isVisible ? "fade-in" : ""}`}
+          onMouseMove={handleMouseMove}
+        >
           <img
-            className="headshot-wrapper__image"
-            // src={headshot}
-            alt="headshot photo of Nick, the one who made this website"
+            className={`hero-wrapper__headshot--img ${
+              isVisible ? "fade-in" : ""
+            }`}
+            src={headshot}
+            alt="headshot of Nick, the one who made this website"
+            style={{
+              boxShadow: `${-borderPosition.x}px ${-borderPosition.y}px 0 2px var(--ice-blue)`,
+            }}
           />
         </div>
-        <div className="landing-page__text-container">
-          <h1 className="landing-page__text-container--h1">
-            Hi, my name is <span className="">Nick</span>
-          </h1>
-          <div className="landing-page__cube-container">
-            <h2 className="landing-page__text-container--h2">I am a</h2>
-            <div className="landing-page__rotating-cube">
+        <div className="landing-page__line--container">
+          <p className="landing-page__line--title">this way</p>
+          <div className="landing-page__line"></div>
+        </div>
+        <div className="hero-wrapper__text">
+          <h1 className="hero-wrapper__text--h1">Hi,</h1>
+          <div className="hero-wrapper__text--group">
+            <h2 className="hero-wrapper__text--h2">my name is</h2>
+            <h2 className="hero-wrapper__text--h2">
+              <span className="hero-wrapper__text--highlight">Nick</span>.
+            </h2>
+          </div>
+          <div className="hero-wrapper__cube-container">
+            <h3 className="hero-wrapper__text--h3">I'm a</h3>
+            <div className="hero-wrapper__rotating-cube">
               <div className="cube">
-                <div className="face front">software engineer</div>
-                <div className="face top">web developer</div>
-                <div className="face back">UX designer</div>
-                <div className="face bottom">creative developer</div>
+                <div className="face front">
+                  software engineer<span className="face__period">.</span>
+                </div>
+                <div className="face bottom">
+                  web developer<span className="face__period">.</span>
+                </div>
+                <div className="face back">
+                  UX designer<span className="face__period">.</span>
+                </div>
+                <div className="face top">
+                  creative<span className="face__period">.</span>
+                </div>
               </div>
             </div>
           </div>
