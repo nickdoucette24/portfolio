2f383fccf92b440335c5e28e76fed8f6f99599b8
Nick Doucette <nickdoucette24@gmail.com>
2024-11-26 21:02:03 -0500

    LandingPage mostly responsive now, few tweaks but pretty good. Decided against the headshot on the mainpage so lots of commented out code for now. Header fixes then My Process section next.

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,65 +1,46 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import "./LandingPage.scss";
-import headshot from "../../assets/images/headshot.JPG";
+// import headshot from "../../assets/images/headshot.JPG";
 
 const LandingPage = () => {
-  const [borderPosition, setBorderPosition] = useState({ x: 0, y: 0 });
-  const [isVisible, setIsVisible] = useState(false);
+  // const [borderPosition, setBorderPosition] = useState({ x: 0, y: 0 });
+  // const [isVisible, setIsVisible] = useState(false);
 
-  const handleMouseMove = (event) => {
-    const img = event.target.getBoundingClientRect();
-    const x = (event.clientX - img.left - img.width / 2) / 20;
-    const y = (event.clientY - img.top - img.height / 2) / 20;
-    setBorderPosition({ x, y });
-  };
+  // const handleMouseMove = (event) => {
+  //   const img = event.target.getBoundingClientRect();
+  //   const x = (event.clientX - img.left - img.width / 2) / 20;
+  //   const y = (event.clientY - img.top - img.height / 2) / 20;
+  //   setBorderPosition({ x, y });
+  // };
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            setIsVisible(true);
-          }
-        });
-      },
-      { threshold: 0.25 }
-    );
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver(
+  //     (entries) => {
+  //       entries.forEach((entry) => {
+  //         if (entry.isIntersecting) {
+  //           setIsVisible(true);
+  //         }
+  //       });
+  //     },
+  //     { threshold: 0.75 }
+  //   );
 
-    const target = document.querySelector(".hero-wrapper__headshot");
-    if (target) {
-      observer.observe(target);
-    }
-    return () => {
-      if (target) {
-        observer.unobserve(target);
-      }
-    };
-  }, []);
+  //   const target = document.querySelector(".hero-wrapper__headshot");
+  //   if (target) {
+  //     observer.observe(target);
+  //   }
+  //   return () => {
+  //     if (target) {
+  //       observer.unobserve(target);
+  //     }
+  //   };
+  // }, []);
 
   return (
     <section className="landing-page">
       <div className="hero-wrapper">
-        <div
-          className={`hero-wrapper__headshot ${isVisible ? "fade-in" : ""}`}
-          onMouseMove={handleMouseMove}
-        >
-          <img
-            className={`hero-wrapper__headshot--img ${
-              isVisible ? "fade-in" : ""
-            }`}
-            src={headshot}
-            alt="headshot of Nick, the one who made this website"
-            style={{
-              boxShadow: `${-borderPosition.x}px ${-borderPosition.y}px 0 2px var(--ice-blue)`,
-            }}
-          />
-        </div>
-        <div className="landing-page__line--container">
-          <p className="landing-page__line--title">this way</p>
-          <div className="landing-page__line"></div>
-        </div>
         <div className="hero-wrapper__text">
-          <h1 className="hero-wrapper__text--h1">Hi,</h1>
+          <h1 className="hero-wrapper__text--h1">Hello,</h1>
           <div className="hero-wrapper__text--group">
             <h2 className="hero-wrapper__text--h2">my name is</h2>
             <h2 className="hero-wrapper__text--h2">
@@ -87,6 +68,28 @@ const LandingPage = () => {
           </div>
         </div>
       </div>
+      <div className="landing-page__line--container">
+        <p className="landing-page__line--title">this way</p>
+        <div className="landing-page__line"></div>
+      </div>
+      {/* <div
+          className={`hero-wrapper__headshot ${isVisible ? "fade-in" : ""}`}
+          onMouseMove={handleMouseMove}
+        >
+          <img
+            className={`hero-wrapper__headshot--img ${
+              isVisible ? "fade-in" : ""
+            }`}
+            src={headshot}
+            alt="headshot of Nick, the one who made this website"
+            style={{
+              boxShadow: `${-borderPosition.x}px ${-borderPosition.y}px 0 2px var(--ice-blue)`,
+            }}
+          />
+          <p className="hero-wrapper__headshot--text">
+            Here's a photo of me looking professional so you know I'm real!
+          </p>
+        </div> */}
     </section>
   );
 };
