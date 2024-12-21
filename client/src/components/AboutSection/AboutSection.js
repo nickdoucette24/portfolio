534f3f5516d2ca749987e1a652a44b4b5881e332
Nick Doucette <nickdoucette24@gmail.com>
2024-12-21 14:45:53 -0500

    Wrote about section intro and initial structure.

---
+++ client/src/components/AboutSection/AboutSection.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./AboutSection.scss";
+
+const AboutSection = () => {
+  return (
+    <div className="about-container">
+      <h3>Have an idea? Let’s bring it to life.</h3>
+      <p>
+        I’m a full-stack software engineer based in Ontario, Canada,
+        specializing in crafting cloud-based solutions from the ground up, with
+        a focus on merging intuitive design with seamless performance.
+      </p>
+    </div>
+  );
+};
+
+export default AboutSection;
--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,44 +1,11 @@
 // import { useState, useEffect } from "react";
+import AboutSection from "../../components/AboutSection/AboutSection";
 import "./LandingPage.scss";
-// import headshot from "../../assets/images/headshot.JPG";
 
 const LandingPage = () => {
-  // const [borderPosition, setBorderPosition] = useState({ x: 0, y: 0 });
-  // const [isVisible, setIsVisible] = useState(false);
-
-  // const handleMouseMove = (event) => {
-  //   const img = event.target.getBoundingClientRect();
-  //   const x = (event.clientX - img.left - img.width / 2) / 20;
-  //   const y = (event.clientY - img.top - img.height / 2) / 20;
-  //   setBorderPosition({ x, y });
-  // };
-
-  // useEffect(() => {
-  //   const observer = new IntersectionObserver(
-  //     (entries) => {
-  //       entries.forEach((entry) => {
-  //         if (entry.isIntersecting) {
-  //           setIsVisible(true);
-  //         }
-  //       });
-  //     },
-  //     { threshold: 0.75 }
-  //   );
-
-  //   const target = document.querySelector(".hero-wrapper__headshot");
-  //   if (target) {
-  //     observer.observe(target);
-  //   }
-  //   return () => {
-  //     if (target) {
-  //       observer.unobserve(target);
-  //     }
-  //   };
-  // }, []);
-
   return (
-    <section className="landing-page">
-      <div className="hero-wrapper">
+    <div className="landing-page">
+      <section className="hero-wrapper">
         <div className="hero-wrapper__text">
           <h1 className="hero-wrapper__text--hello">Hello,</h1>
           <div className="hero-wrapper__text--group">
@@ -67,30 +34,15 @@ const LandingPage = () => {
             </div>
           </div>
         </div>
-      </div>
+      </section>
       <div className="landing-page__line-container">
         <p className="landing-page__line-container--title">this way</p>
         <div className="landing-page__line-container--line"></div>
       </div>
-      {/* <div
-          className={`hero-wrapper__headshot ${isVisible ? "fade-in" : ""}`}
-          onMouseMove={handleMouseMove}
-        >
-          <img
-            className={`hero-wrapper__headshot--img ${
-              isVisible ? "fade-in" : ""
-            }`}
-            src={headshot}
-            alt="headshot of Nick, the one who made this website"
-            style={{
-              boxShadow: `${-borderPosition.x}px ${-borderPosition.y}px 0 2px var(--ice-blue)`,
-            }}
-          />
-          <p className="hero-wrapper__headshot--text">
-            Here's a photo of me looking professional so you know I'm real!
-          </p>
-        </div> */}
-    </section>
+      <section className="about-wrapper">
+        <AboutSection />
+      </section>
+    </div>
   );
 };
 
